2fa0c3aae3ca84157439043e3d4c65c8de7a9712
Sven Fuchs <svenfuchs@artweb-design.de>
2010-01-04 08:23:53 +0100

    be able to specify an export target directory. default to ./data

--- lib/cldr.rb
+++ lib/cldr.rb
@@ -7,9 +7,9 @@ class Cldr < Thor
   autoload :Export, 'cldr/export'
   autoload :Format, 'cldr/format'
 
-  desc "export [--locales=de fr en] [--components=numbers plurals]", 
-       "Export CLDR data by locales and components"
-  method_options %w(locales -l components -c) => :array
+  desc "export [--locales=de fr en] [--components=numbers plurals] [--target=./data]",
+       "Export CLDR data by locales and components to target dir"
+  method_options %w(locales -l components -c) => :array, %w(target -t) => :string
 
   def export
     $stdout.sync
--- lib/cldr/export.rb
+++ lib/cldr/export.rb
@@ -1,4 +1,3 @@
-require 'fileutils'
 require 'core_ext/string/camelize'
 require 'core_ext/string/underscore'
 require 'core_ext/hash/deep_stringify_keys'
@@ -7,10 +6,10 @@ class Cldr
   module Export
     autoload :Ruby, 'cldr/export/ruby'
     autoload :Yaml, 'cldr/export/yaml'
-    
+
     class << self
       def base_path
-        @@base_path ||= File.expand_path(File.dirname(__FILE__) + '/../../data')
+        @@base_path ||= File.expand_path('./data')
       end
 
       def base_path=(base_path)
@@ -18,9 +17,10 @@ class Cldr
       end
 
       def export(options = {}, &block)
-        format     = options[:format]
-        locales    = options[:locales]    || Data.locales
-        components = options[:components] || Data.components
+        format         = options[:format]
+        locales        = options[:locales]    || Data.locales
+        components     = options[:components] || Data.components
+        self.base_path = options[:target] if options[:target]
 
         locales.each do |locale|
           components.each do |component|
@@ -28,22 +28,22 @@ class Cldr
           end
         end
       end
-      
+
       def exporter(component, format)
         name = format ? format : component.to_s == 'Plurals' ? 'ruby' : 'yaml'
         const_get(name.to_s.camelize).new
       end
-      
+
       def data(component, locale)
         Data.const_get(component.to_s.camelize).new(locale)
       end
-      
+
       def write(path, data)
         FileUtils.rm(path) if File.exists?(path)
         FileUtils.mkdir_p(File.dirname(path))
         File.open(path, 'w+') { |f| f.write(data) }
       end
-    
+
       def path(locale, component, extension)
         "#{Export.base_path}/#{locale.gsub('_', '-')}/#{component.to_s.underscore}.#{extension}"
       end
