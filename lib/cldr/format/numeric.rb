30675426a09abd3183eadf8e9d0083acd2997d9c
Sven Fuchs <svenfuchs@artweb-design.de>
2010-01-10 15:05:37 +0100

    refactor Numeric format

--- lib/cldr/format/numeric.rb
+++ lib/cldr/format/numeric.rb
@@ -1,20 +1,21 @@
 class Cldr
   module Format
     class Numeric < Base
-      attr_reader :formats, :symbols, :precision
+      attr_reader :prefix, :suffix, :integer_format, :fraction_format, :symbols
+
+      DEFAULT_SYMBOLS = { :group => ',', :decimal => '.', :plus_sign => '+', :minus_sign => '-' }
 
       def initialize(format, symbols = {})
-        @symbols   = { :group => ',', :decimal => '.', :plus_sign => '+', :minus_sign => '-' }.merge(symbols)
-        @formats   = parse_format(format, symbols)
-        @precision = formats[:fraction] ? formats[:fraction].precision : 0
+        @symbols = DEFAULT_SYMBOLS.merge(symbols)
+        @prefix, @suffix, @integer_format, @fraction_format = *parse_format(format, symbols)
       end
 
       def apply(number, options = {})
         int, fraction = parse_number(number, options)
 
-        result =  formats[:integer].apply(int, options)
-        result << formats[:fraction].apply(fraction, options) if formats[:fraction] && fraction
-        formats[:prefix] + result + formats[:suffix]
+        result =  integer_format.apply(int, options)
+        result << fraction_format.apply(fraction, options) if fraction_format && fraction
+        prefix + result + suffix
       end
 
       protected
@@ -22,16 +23,11 @@ class Cldr
         def parse_format(format, symbols = {})
           format =~ /^([^0#,\.]*)([0#,\.]+)([^0#,\.]*)$/
           int, fraction = $2.split('.')
-          { 
-            :prefix   => $1.to_s,
-            :suffix   => $3.to_s,
-            :integer  => Integer.new(int, symbols),
-            :fraction => Fraction.new(fraction, symbols)
-          }
+          [$1.to_s, $3.to_s, Integer.new(int, symbols), Fraction.new(fraction, symbols)]
         end
-        
+
         def parse_number(number, options = {})
-          precision = options[:precision] || self.precision
+          precision = options[:precision] || fraction_format.precision
           number = round_to(number, precision)
           number.abs.to_s.split('.')
         end
