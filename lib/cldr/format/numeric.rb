65424ec36e66ee620bf94435fb5789cfcc8f6407
Sven Fuchs <svenfuchs@artweb-design.de>
2010-01-10 15:28:01 +0100

    refactor numeric format

--- lib/cldr/format/numeric.rb
+++ lib/cldr/format/numeric.rb
@@ -4,6 +4,7 @@ class Cldr
       attr_reader :prefix, :suffix, :integer_format, :fraction_format, :symbols
 
       DEFAULT_SYMBOLS = { :group => ',', :decimal => '.', :plus_sign => '+', :minus_sign => '-' }
+      FORMAT_PATTERN  = /^([^0#,\.]*)([0#,\.]+)([^0#,\.]*)$/
 
       def initialize(format, symbols = {})
         @symbols = DEFAULT_SYMBOLS.merge(symbols)
@@ -14,14 +15,14 @@ class Cldr
         int, fraction = parse_number(number, options)
 
         result =  integer_format.apply(int, options)
-        result << fraction_format.apply(fraction, options) if fraction_format && fraction
+        result << fraction_format.apply(fraction, options) if fraction
         prefix + result + suffix
       end
 
       protected
 
         def parse_format(format, symbols = {})
-          format =~ /^([^0#,\.]*)([0#,\.]+)([^0#,\.]*)$/
+          format =~ FORMAT_PATTERN
           int, fraction = $2.split('.')
           [$1.to_s, $3.to_s, Integer.new(int, symbols), Fraction.new(fraction, symbols)]
         end
