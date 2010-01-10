0b9e3f7d30666148a79a2db268fab10318d771b3
Sven Fuchs <svenfuchs@artweb-design.de>
2010-01-10 14:58:13 +0100

    don't alter the passed string in #interpolate

--- lib/cldr/format/base.rb
+++ lib/cldr/format/base.rb
@@ -2,9 +2,11 @@ class Cldr
   module Format
     class Base
       def interpolate(string, value, orientation = :right)
+        string = string.dup
         value  = value.to_s
         length = value.length
         start, pad = orientation == :left ? [0, :rjust] : [-length, :ljust]
+
         string = string.send(pad, length, '#') if string.length < length
         string[start, length] = value
         string.gsub('#', '')
