e551907c5a4369420ba87f18153cf203bd8209ee
Sven Fuchs <svenfuchs@artweb-design.de>
2010-01-14 23:37:59 +0100

    fix deep_merge

--- lib/core_ext/hash/deep_merge.rb
+++ lib/core_ext/hash/deep_merge.rb
@@ -1,6 +1,6 @@
 class Hash
   def deep_merge(other)
-    merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
+    merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : (v2 || v1) }
     merge(other, &merger)
   end
 end
