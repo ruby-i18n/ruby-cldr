fbd966f189981f32028cc9f78f180f07960f1522
Sven Fuchs <svenfuchs@artweb-design.de>
2010-01-16 15:12:26 +0100

    use ljust instead of pad

--- lib/cldr/format/decimal/base.rb
+++ lib/cldr/format/decimal/base.rb
@@ -3,12 +3,12 @@ class Cldr
     class Decimal
       class Base
         def interpolate(string, value, orientation = :right)
-          string = string.dup
           value  = value.to_s
           length = value.length
           start, pad = orientation == :left ? [0, :rjust] : [-length, :ljust]
 
-          string = string.send(pad, length, '#') if string.length < length
+          string = string.dup
+          string = string.ljust(length, '#') if string.length < length
           string[start, length] = value
           string.gsub('#', '')
         end
