eb1fad110a81d6c6a2e0c3fd33a2948428793d3b
Sven Fuchs <svenfuchs@artweb-design.de>
2010-01-18 08:35:25 +0100

    disable datetime formats as we can just use plain interpolation in I18n (might be resurrected at some point)

--- lib/cldr/format/datetime.rb
+++ lib/cldr/format/datetime.rb
@@ -3,26 +3,18 @@ class Cldr
     class Datetime
       autoload :Base, 'cldr/format/datetime/base'
 
-      attr_reader :date, :time
-
-      def initialize(format, date, time)
-        @date, @time = date, time
-        compile(format)
-      end
-
-      protected
-
-        def compile(format)
-          (class << self; self; end).class_eval <<-code
-            def apply(datetime, options = {}); #{compile_format(format)}; end
-          code
-        end
-        
-        def compile_format(format)
-          "'" + format.gsub(%r(\{(0|1)\})) do |token|
-            "' + #{token == '{0}' ? 'time' : 'date'}.apply(datetime, options) + '"
-          end + "'"
-        end
+      # attr_reader :format, :date, :time
+      # 
+      # def initialize(format, date, time)
+      #   @format, @date, @time = date, date, time
+      # end
+      # 
+      # def apply(datetime, options = {})
+      #   format.dup % {
+      #     :date => options[:date] || date.apply(datetime, options),
+      #     :time => options[:time] || time.apply(datetime, options)
+      #   }
+      # end
     end
   end
 end
