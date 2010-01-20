50e4705a410c24057295525422a87a6d6d28ee50
Sven Fuchs <svenfuchs@artweb-design.de>
2010-01-20 11:22:35 +0100

    improve .autotest

--- test/test_autotest.rb
+++ test/test_autotest.rb
@@ -7,6 +7,8 @@ def tests_for(filename)
     "test/#{dir}/{#{base},#{base}/**/*}_test.rb"
   when %r(lib/cldr/.*\.rb)
     "test/" + filename.gsub('lib/cldr/', '').gsub(/\.rb/, '_test.rb')
+  when %r(^test/.*_test\.rb)
+    filename
   end
   pattern ? Dir[pattern.gsub('//', '/')] : []
 end
@@ -15,10 +17,14 @@ if $0 == __FILE__
   require 'test/unit'
   
   class TestAutotestMatching < Test::Unit::TestCase
-    define_method :"test: works" do
+    define_method :"test: default mapping for library files" do
       assert tests_for("lib/cldr/format/date.rb").all? { |file| file =~ /date_test.rb/ }
       assert tests_for("lib/cldr/format/decimal/fraction.rb").all? { |file| file =~ /fraction_test.rb/ }
       assert tests_for("lib/cldr/format/decimal/base.rb").all? { |file| file =~ /decimal/ }
+    end
+
+    define_method :"test: default mapping for test files" do
+      assert_equal ["test/export_test.rb"], tests_for("test/export_test.rb")
     end
   end
 end
