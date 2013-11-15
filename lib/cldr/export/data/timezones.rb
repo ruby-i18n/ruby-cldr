08a3fb01e0205892314085c833fa1d7f48f52f24
Dāvis <davispuh@gmail.com>
2013-11-15 21:21:03 +0200

    Improve Timezone support

--- lib/cldr/export/data/timezones.rb
+++ lib/cldr/export/data/timezones.rb
@@ -6,21 +6,44 @@ module Cldr
       class Timezones < Base
         def initialize(locale)
           super
-          update(:timezones => timezones)
+          update(:timezones => timezones, :metazones => metazones)
         end
 
         def timezones
           @timezones ||= select('dates/timeZoneNames/zone').inject({}) do |result, zone|
-            type = zone.attribute('type').value.to_sym
-            city = select(zone, 'exemplarCity').first
+            type = zone.attr('type').to_sym
             result[type] = {}
-            # see en.xml, Europe/London, does not have an exemplarCity element
-            # instead it has long and short daylight names which otherwise only
-            # have metazones. (??)
+            long = nodes_to_hash(zone.xpath('long/*'))
+            result[type][:long] = long unless long.empty?
+            short = nodes_to_hash(zone.xpath('short/*'))
+            result[type][:short] = short unless short.empty?
+            city = select(zone, 'exemplarCity').first
             result[type][:city] = city.content if city
             result
           end
         end
+
+        def metazones
+          @metazones ||= select('dates/timeZoneNames/metazone').inject({}) do |result, zone|
+            type = zone.attr('type').to_sym
+            result[type] = {}
+            long = nodes_to_hash(zone.xpath('long/*'))
+            result[type][:long] = long unless long.empty?
+            short = nodes_to_hash(zone.xpath('short/*'))
+            result[type][:short] = short unless short.empty?
+            result
+          end
+        end
+
+        protected
+
+        def nodes_to_hash(nodes)
+          nodes.inject({}) do |result, node|
+            result[node.name.to_sym] = node.content
+            result
+          end
+        end
+
       end
     end
   end
