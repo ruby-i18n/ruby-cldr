75c9b77d749432791b1405859913eb3e0deccec9
Cameron Dutro <camertron@gmail.com>
2015-09-21 23:51:16 -0500

    Minor adjustment

--- lib/cldr/export/data/characters.rb
+++ lib/cldr/export/data/characters.rb
@@ -4,16 +4,24 @@ module Cldr
       class Characters < Base
         def initialize(locale)
           super
-          update(:exemplars => exemplars)
+          update(:characters => { :exemplars => exemplars })
         end
 
         def exemplars
-          select('characters/exemplarCharacters').map do |node|
+          select('/ldml/characters/exemplarCharacters').map do |node|
             {
               # remove enclosing brackets
               characters: node.content[1..-2],
-              type: node.attribute('type').value
+              type: type_from(node)
             }
+          end
+        end
+
+        protected
+
+        def type_from(node)
+          if attrib = node.attribute('type')
+            attrib.value
           end
         end
       end
