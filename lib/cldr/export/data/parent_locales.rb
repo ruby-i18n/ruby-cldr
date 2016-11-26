6be5cb76421e44d0faea448ded653813e991f583
Nate Mueller <nate@retailnext.net>
2016-11-25 20:59:52 -0800

    Add support for parentLocales
    Use the parentLocales section to figure out the correct fallback
    locales.  Can not use customization feature of I18n::Locale::Fallbacks
    since it always places the default parent local before any customizations.

---
+++ lib/cldr/export/data/parent_locales.rb
@@ -0,0 +1,23 @@
+require 'nokogiri'
+
+module Cldr
+  module Export
+    module Data
+      class ParentLocales < Hash
+        def initialize
+          path = File.join(Cldr::Export::Data.dir, 'supplemental', 'supplementalData.xml')
+          doc = File.open(path) { |file| Nokogiri::XML(file) }
+
+          doc.xpath('//parentLocales/parentLocale').each do |node|
+            parent = node.attr('parent')
+            locales = node.attr('locales').split(' ')
+
+            locales.each do |locale|
+              self[locale] = parent
+            end
+          end
+        end
+      end
+    end
+  end
+end
