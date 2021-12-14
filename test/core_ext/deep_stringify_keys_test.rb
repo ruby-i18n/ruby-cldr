50a832522fceaa8135bd35362531487dd1e2b8bc
Michael Overmeyer <michael.overmeyer@shopify.com>
2021-12-14 15:37:03 -0500

    Add test for `deep_stringify_keys`

---
+++ test/core_ext/deep_stringify_keys_test.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+require File.expand_path(File.join(File.dirname(__FILE__) + "/../test_helper"))
+require File.expand_path(File.join(File.dirname(__FILE__) + "/../../lib/core_ext/hash/deep_stringify_keys"))
+
+class TestDeepPrune < Test::Unit::TestCase
+  test "deep_stringify_keys doesn't affect values" do
+    input = {
+      foo: :bar,
+      parent: {
+        nested: :baz,
+      },
+    }
+    expected = {
+      "foo" => :bar,
+      "parent" => {
+        "nested" => :baz,
+      },
+    }
+    assert_equal expected, input.deep_stringify_keys
+  end
+end
