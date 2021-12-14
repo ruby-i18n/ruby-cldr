758f56b7c35bca6af1ec609dd167263b77cb4ddb
Michael Overmeyer <michael.overmeyer@shopify.com>
2021-12-13 19:51:29 -0500

    Ignore `Security/Eval` in `test/export/data/plurals_test.rb`
    We are parsing rules that we generated, so they better be safe to parse 😛

--- test/export/data/plurals_test.rb
+++ test/export/data/plurals_test.rb
@@ -12,11 +12,11 @@ class TestCldrDataPluralParser < Test::Unit::TestCase
   end
 
   def test_compiles_to_valid_ruby_code
-    assert_nothing_raised { eval(cldr_rules.to_ruby) }
+    assert_nothing_raised { eval(cldr_rules.to_ruby) } # rubocop:disable Security/Eval
   end
 
   def test_evals_to_a_hash_containing_plural_rule_and_keys_per_locale
-    data = eval(cldr_rules.to_ruby)
+    data = eval(cldr_rules.to_ruby) # rubocop:disable Security/Eval
     assert(Hash === data)
     assert(Proc === data[:de][:i18n][:plural][:rule])
     assert_equal([:one, :other], data[:de][:i18n][:plural][:keys])
@@ -175,7 +175,7 @@ class TestCldrDataPluralParser < Test::Unit::TestCase
 
   def test_eval_n_in
     n = 3.3
-    assert_equal(false, eval(Cldr::Export::Data::Plurals::Rule.parse("n mod 100 in 3..6").to_ruby, binding))
+    assert_equal(false, eval(Cldr::Export::Data::Plurals::Rule.parse("n mod 100 in 3..6").to_ruby, binding)) # rubocop:disable Security/Eval
   end
 
   def test_codes_with_missing_spaces
@@ -187,7 +187,7 @@ class TestCldrDataPluralParser < Test::Unit::TestCase
   def test_n_negative
     # one: i = 1 and v = 0 @integer 1
     # other: @integer 0, 2~16, 100, 1000, 10000, 100000, 1000000, … @decimal 0.0~1.5, 10.0, 100.0, 1000.0, 10000.0, 100000.0, 1000000.0, …
-    fn = eval(cldr_rules.rule(:de).to_ruby)
+    fn = eval(cldr_rules.rule(:de).to_ruby) # rubocop:disable Security/Eval
     assert_equal(:one, fn.call(-1))
     assert_equal(:one, fn.call("-1"))
     assert_equal(:one, fn.call(1))
@@ -199,7 +199,7 @@ class TestCldrDataPluralParser < Test::Unit::TestCase
   def test_n_digit
     # one: n = 0..1 or n = 11..99 @integer 0, 1, 11~24 @decimal 0.0, 1.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0
     # other: @integer 2~10, 100~106, 1000, 10000, 100000, 1000000, … @decimal 0.1~0.9, 1.1~1.7, 10.0, 100.0, 1000.0, 10000.0, 100000.0, 1000000.0, …
-    fn = eval(cldr_rules.rule(:tzm).to_ruby)
+    fn = eval(cldr_rules.rule(:tzm).to_ruby) # rubocop:disable Security/Eval
     assert_equal(:one, fn.call(0))
     assert_equal(:one, fn.call(1))
     assert_equal(:one, fn.call(11))
@@ -214,7 +214,7 @@ class TestCldrDataPluralParser < Test::Unit::TestCase
   def test_n_string
     # one: i = 1 and v = 0 or i = 0 and t = 1 @integer 1 @decimal 0.1, 0.01, 0.10, 0.001, 0.010, 0.100, 0.0001, 0.0010, 0.0100, 0.1000
     # other: @integer 0, 2~16, 100, 1000, 10000, 100000, 1000000, … @decimal 0.0, 0.2~1.6, 10.0, 100.0, 1000.0, 10000.0, 100000.0, 1000000.0, …
-    fn = eval(cldr_rules.rule(:pt).to_ruby)
+    fn = eval(cldr_rules.rule(:pt).to_ruby) # rubocop:disable Security/Eval
     assert_equal(:one, fn.call("1"))
     assert_equal(:one, fn.call("0.00100"))
     assert_equal(:one, fn.call("-0.01"))
@@ -228,7 +228,7 @@ class TestCldrDataPluralParser < Test::Unit::TestCase
     # one: v = 0 and i % 10 = 1 and i % 100 != 11 or f % 10 = 1 and f % 100 != 11 @integer 1, 21, 31, 41, 51, 61, 71, 81, 101, 1001, … @decimal 0.1, 1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 7.1, 10.1, 100.1, 1000.1, …
     # few: v = 0 and i % 10 = 2..4 and i % 100 != 12..14 or f % 10 = 2..4 and f % 100 != 12..14 @integer 2~4, 22~24, 32~34, 42~44, 52~54, 62, 102, 1002, … @decimal 0.2~0.4, 1.2~1.4, 2.2~2.4, 3.2~3.4, 4.2~4.4, 5.2, 10.2, 100.2, 1000.2, …
     # other: @integer 0, 5~19, 100, 1000, 10000, 100000, 1000000, … @decimal 0.0, 0.5~1.0, 1.5~2.0, 2.5~2.7, 10.0, 100.0, 1000.0, 10000.0, 100000.0, 1000000.0, …
-    fn = eval(cldr_rules.rule(:hr).to_ruby)
+    fn = eval(cldr_rules.rule(:hr).to_ruby) # rubocop:disable Security/Eval
     assert_equal(:one, fn.call(1))
     assert_equal(:one, fn.call("21"))
     assert_equal(:one, fn.call("131"))
