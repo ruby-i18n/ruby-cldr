c82ac89ce0187115c496940c4c5a8829c30fe2ba
Michael Overmeyer <michael.overmeyer@shopify.com>
2021-12-14 15:42:35 -0500

    Add `deep_stringify` and `deep_stringify_values`

--- lib/core_ext/hash/deep_stringify.rb
+++ lib/core_ext/hash/deep_stringify.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 class Hash
-  def deep_stringify_keys
+  def deep_stringify(stringify_keys: true, stringify_values: true)
     each_with_object({}) do |(key, value), result|
-      value = value.deep_stringify_keys if value.is_a?(Hash)
-      key = key.to_s if key.is_a?(Symbol)
+      value = value.deep_stringify(stringify_keys: stringify_keys, stringify_values: stringify_values) if value.is_a?(Hash)
+      key = key.to_s if stringify_keys && key.is_a?(Symbol)
+      value = value.to_s if stringify_values && value.is_a?(Symbol)
       result[key] = value
     end
+  end
+
+  def deep_stringify_keys
+    deep_stringify(stringify_keys: true, stringify_values: false)
+  end
+
+  def deep_stringify_values
+    deep_stringify(stringify_keys: false, stringify_values: true)
   end
 end
--- test/core_ext/deep_stringify_test.rb
+++ test/core_ext/deep_stringify_test.rb
@@ -5,6 +5,22 @@ require File.expand_path(File.join(File.dirname(__FILE__) + "/../test_helper"))
 require File.expand_path(File.join(File.dirname(__FILE__) + "/../../lib/core_ext/hash/deep_stringify"))
 
 class TestDeepPrune < Test::Unit::TestCase
+  test "deep_stringify affects both keys and values" do
+    input = {
+      foo: :bar,
+      parent: {
+        nested: :baz,
+      },
+    }
+    expected = {
+      "foo" => "bar",
+      "parent" => {
+        "nested" => "baz",
+      },
+    }
+    assert_equal expected, input.deep_stringify
+  end
+
   test "deep_stringify_keys doesn't affect values" do
     input = {
       foo: :bar,
@@ -19,5 +35,21 @@ class TestDeepPrune < Test::Unit::TestCase
       },
     }
     assert_equal expected, input.deep_stringify_keys
+  end
+
+  test "deep_stringify_values doesn't affect keys" do
+    input = {
+      foo: :bar,
+      parent: {
+        nested: :baz,
+      },
+    }
+    expected = {
+      foo: "bar",
+      parent: {
+        nested: "baz",
+      },
+    }
+    assert_equal expected, input.deep_stringify_values
   end
 end
