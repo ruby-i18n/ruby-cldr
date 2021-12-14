9c4012da5c56b6d78c15715b4e977d356ccdd9ea
Michael Overmeyer <michael.overmeyer@shopify.com>
2021-12-13 19:25:11 -0500

    Autocorrect `Style/ClassAndModuleChildren`

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,20 +6,24 @@ require "test/unit"
 require "cldr"
 require "rubygems"
 
-class Test::Unit::TestCase
-  def self.test(name, &block)
-    test_name = "test_#{name.gsub(/\s+/, "_")}".to_sym
-    defined = begin
-      instance_method(test_name)
-    rescue
-      false
-    end
-    raise "#{test_name} is already defined in #{self}" if defined
-    if block_given?
-      define_method(test_name, &block)
-    else
-      define_method(test_name) do
-        flunk("No implementation provided for #{name}")
+module Test
+  module Unit
+    class TestCase
+      def self.test(name, &block)
+        test_name = "test_#{name.gsub(/\s+/, "_")}".to_sym
+        defined = begin
+          instance_method(test_name)
+        rescue
+          false
+        end
+        raise "#{test_name} is already defined in #{self}" if defined
+        if block_given?
+          define_method(test_name, &block)
+        else
+          define_method(test_name) do
+            flunk("No implementation provided for #{name}")
+          end
+        end
       end
     end
   end
