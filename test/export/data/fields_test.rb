fd96fbf2dfef74fbbe207cb38257b5c579300c29
Daniel Holker <daniel.holker@shopify.com>
2022-06-20 14:58:14 -0400

    Implement alias in Fields

---
+++ test/export/data/fields_test.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+require File.expand_path(File.join(File.dirname(__FILE__) + "/../../test_helper"))
+
+class TestCldrDataFields < Test::Unit::TestCase
+  test "Alias nodes are exported as paths to their targets" do
+    data = Cldr::Export::Data::Fields.new(:root)
+    path = data.dig(:fields, :"day-narrow")
+    assert_equal :"fields.day-short", path
+
+    path = data.dig(*split_path_string(path))
+    assert_equal :"fields.day", path
+
+    day = data.dig(*split_path_string(path))
+    assert_not_nil day
+  end
+
+  private
+
+  def split_path_string(path)
+    path.to_s.split(".").map(&:to_sym)
+  end
+end
