68cab4d8a688cabdc5f28c6b7ca63c2eb84a8c4d
Daniel Holker <daniel.holker@shopify.com>
2022-06-22 13:54:27 -0400

    Implement alias in Lists

--- lib/cldr/export/data/lists.rb
+++ lib/cldr/export/data/lists.rb
@@ -18,11 +18,33 @@ module Cldr
             else
               :default
             end
-
-            list_pattern_ret[pattern_type] = select(list_pattern, "listPatternPart").each_with_object({}) do |part, part_ret|
-              part_ret[part.attribute("type").value.to_sym] = part.content
-            end
+            list_pattern_ret[pattern_type] = list_pattern(list_pattern)
           end
+        end
+
+        def list_pattern(list_pattern)
+          aliased = select(list_pattern, "alias").first
+          if aliased
+            xpath_to_key(aliased.attribute("path").value)
+          else
+            get_pattern_parts(list_pattern)
+          end
+        end
+
+        def get_pattern_parts(list_pattern)
+          select(list_pattern, "listPatternPart").each_with_object({}) do |part, part_ret|
+            part_ret[part.attribute("type").value.to_sym] = part.content
+          end
+        end
+
+        def xpath_to_key(xpath)
+          return :"lists.default" if xpath == "../listPattern"
+
+          match = xpath.match(%r{^\.\./listPattern\[@type='([^']*)'\]$})
+          raise StandardError, "Didn't find expected data in alias path attribute: #{xpath}" unless match
+
+          type = match[1]
+          :"lists.#{type}"
         end
       end
     end
---
+++ test/export/data/lists_test.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+require File.expand_path(File.join(File.dirname(__FILE__) + "/../../test_helper"))
+
+class TestCldrDataLists < Test::Unit::TestCase
+  test "Alias nodes are exported as paths to their targets" do
+    data = Cldr::Export::Data::Lists.new(:root)
+    path = data.dig(:lists, :"or-narrow")
+    assert_equal :"lists.or-short", path
+
+    path = data.dig(*split_path_string(path))
+    assert_equal :"lists.or", path
+
+    pattern_or = data.dig(*split_path_string(path))
+    assert_not_nil pattern_or
+  end
+
+  private
+
+  def split_path_string(path)
+    path.to_s.split(".").map(&:to_sym)
+  end
+end
